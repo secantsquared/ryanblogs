136a5036c75f9b5bcd4d46d0aeb0aea0403f7608
Ryan Boris <ryan.boris@gmail.com>
2020-02-26 08:33:58 -0500

    fix links

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,6 +32,7 @@ export default class extends Document<Props> {
       <html lang={language}>
         <Head>
           {/*Global meta tags*/}
+          <title>RB Blogs Sometimes</title>
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <base href="/" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
